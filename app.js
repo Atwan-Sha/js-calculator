81846308a5737cad8ef3b9a803a209016d4cd8a0
Atwan-Sha <atwanshaaban@gmail.com>
2022-10-25 11:19:21 +0300

    finished event handlers

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
+// *CALCULATOR APP SCRIPT
 
+// *Operators
 function add(x, y){ return x + y; }
-
 function subtract(x, y){ return x - y; }
-
 function multiply(x, y){ return x * y; }
-
 function divide(x, y){ return x / y; }
-
-// function testOperators(){
-//     console.log('add 4+5 = ' + add(4,5));
-//     console.log('subtract 9-10 = ' + subtract(9,10));
-//     console.log('multiply 4*5 = ' + multiply(4,5));
-//     console.log('divide 10/2 = ' + divide(10,2));
-// }
 
 function operate(op, x, y){
     let result;
@@ -32,57 +24,93 @@ function operate(op, x, y){
     return result;
 }
 
-const buttonListNum = document.querySelectorAll('button.num');
-const buttonListOp = document.querySelectorAll('button.op');
-const clearButton = document.querySelector('button.clear');
-const equalsButton = document.querySelector('button.equals');
-
-const display = document.querySelector('.display');
-
+// *Data Storage
 let userInputNum;
 let inputX = null;
 let inputY = null;
-let inputOp;
-let result;
+let inputOp = null;
+//let checkDisplayFill = true;
+let checkOpSelect = false;
 let displayValue = '';
 let displayBuffer = '';
 
+// *DOM Nodes
+const buttonListNum = document.querySelectorAll('button.num');
+const buttonListOp = document.querySelectorAll('button.op');
+const clearButton = document.querySelector('button#clear');
+const equalsButton = document.querySelector('button#equals');
+const display = document.querySelector('.display');
+
+// *Event Handlers:
+// number buttons
 for(let button of buttonListNum){
     button.addEventListener('click', () => {
-        displayBuffer += button.textContent;
-        display.textContent = displayValue + displayBuffer;
+        // update display
+        if(display.textContent.length < 15){
+            displayBuffer += button.textContent;
+            display.textContent = displayValue + displayBuffer;
+            checkOpSelect = true;
+        }
     });
 }
-
+// operator buttons
 for(let button of buttonListOp){
     button.addEventListener('click', () => {
-        userInputNum = Number(displayBuffer);
-        inputOp = button.id;
-        if(inputX == null){
-            inputX = userInputNum;
-        }else{
-            inputY = userInputNum;
-            inputX = operate(inputOp, inputX, inputY);
+        if(checkOpSelect){
+        // calculation
+            userInputNum = Number(displayBuffer);
+            if(inputX == null){
+                inputX = userInputNum;
+            }else{
+                inputY = userInputNum;
+                inputX = operate(inputOp, inputX, inputY);
+            }
+            inputOp = button.id;
+            // update display
+            displayBuffer += button.textContent;
+            displayValue += displayBuffer;
+            display.textContent = displayValue;
+            displayBuffer = '';
+            checkOpSelect = false;
         }
-
-        displayBuffer += button.textContent;
-        displayValue += displayBuffer;
-        display.textContent = displayValue;
-        displayBuffer = '';
-
-        console.log({userInputNum, 
-                     inputX, 
-                     inputY, 
-                     displayValue, 
-                     displayBuffer, 
-                     inputOp
-        });
+        // console.log({userInputNum, 
+        //              inputX, 
+        //              inputY, 
+        //              displayValue, 
+        //              displayBuffer, 
+        //              inputOp
+        // });
 
     });
 }
+// clear button
+clearButton.addEventListener('click', () => {
+    inputX = null;
+    inputY = null;
+    inputOp = null;
+    displayValue = '';
+    displayBuffer = '';
+    display.textContent = '0';
+    checkOpSelect = false;
+});
+// equals button
+equalsButton.addEventListener('click', () => {
+    userInputNum = Number(displayBuffer);
+    if(inputX == null){
+        display.textContent = userInputNum;
+    }else if(displayBuffer == ''){
+        display.textContent = inputX;
+    }else{
+        inputY = userInputNum;
+        inputX = operate(inputOp, inputX, inputY);
+        display.textContent = inputX;
+    }
+});
 
 
-
+// ! Check for display overflow (especially decimals)
+// ! Divide-by-zero snarky error message
+// ! Add display for partial calculations
 
 
 
